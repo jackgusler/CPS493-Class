2fd17d776c7a25ee6fcb7da7ce8449b96e1aead0
jackgusler <guslerj1@newpaltz.edu>
2023-10-07 12:02:34 -0400

    followed the in class lecture - 10/5

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
+
+const isActive = ref(false);
+
+</script>
+
+<template>
+    <div>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+              <a class="navbar-item" href="https://bulma.io">
+                <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+              </a>
+          
+              <a role="button" class="navbar-burger" :class="{ 'is-active': isActive }" @click="isActive = !isActive" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            </div>
+          
+            <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': isActive }">
+              <div class="navbar-start">
+                <RouterLink class="navbar-item" to="/" >Home</RouterLink>
+                <RouterLink class="navbar-item" to="/about">About</RouterLink>
+          
+                <div class="navbar-item has-dropdown is-hoverable">
+                  <a class="navbar-link">
+                    More
+                  </a>
+          
+                  <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                      About
+                    </a>
+                    <a class="navbar-item">
+                      Jobs
+                    </a>
+                    <a class="navbar-item">
+                      Contact
+                    </a>
+                    <hr class="navbar-divider">
+                    <a class="navbar-item">
+                      Report an issue
+                    </a>
+                  </div>
+                </div>
+              </div>
+          
+              <div class="navbar-end">
+                <div class="navbar-item">
+                  <div class="buttons">
+                    <a class="button is-primary">
+                      <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light">
+                      Log in
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+    </div>
+</template>
+
+<style scoped>
+.router-link-active {
+    font-weight: bold;
+    color: #00d1b2;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+const newTask = ref('');
+const tasks = ref([] as string[]);
+
+function addTask() {
+  tasks.value.push(newTask.value);
+  newTask.value = '';
+};
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="title">Home</h1>
+    <h2 class="subtitle">Home Page</h2>
+
+    <article class="panel is-primary">
+      <p class="panel-heading">Primary</p>
+      <p class="panel-tabs">
+        <a class="is-active">All</a>
+        <a>Public</a>
+        <a>Private</a>
+        <a>Sources</a>
+        <a>Forks</a>
+      </p>
+      <div class="panel-block">
+        <p class="control has-icons-left">
+          <input class="input is-primary" type="text" placeholder="Search" @keypress.enter="addTask" v-model="newTask" />
+          <span class="icon is-left">
+            <i class="fas fa-search" aria-hidden="true"></i>
+          </span>
+        </p>
+      </div>
+      <label class="panel-block" v-for="task in tasks">
+        <input type="checkbox">
+          { task }
+      </label>
+    </article>
   </main>
 </template>
+
+<style scoped>
+.container {
+  max-width: 960px;
+}
+</style>
