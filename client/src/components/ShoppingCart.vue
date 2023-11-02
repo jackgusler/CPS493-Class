930f7d672b9a03a7c0d07e2f6d811dbaa24ef65d
jackgusler <guslerj1@newpaltz.edu>
2023-11-02 14:34:31 -0400

    fixed shopping cart css

--- client/src/components/ShoppingCart.vue
+++ client/src/components/ShoppingCart.vue
@@ -2,8 +2,8 @@
 import {
   type ShoppingCartItem,
   addToCart,
-  removeFromCart,
   getCart,
+  removeFromCart,
   total,
   count,
 } from "@/model/shoppingCart";
@@ -12,33 +12,30 @@ const cart = getCart();
 </script>
 
 <template>
-  <div>
+  <div class="cart">
     <h1 class="subtitle">Cart: {{ count }} items, total: ${{ total }}</h1>
-    <div class="cart">
-      <div class="cart-items">
-        <div v-for="item in cart" :key="item.product.id" class="item">
-          <img :src="item.product.thumbnail" :alt="item.product.title" />
-          <div class="item-details">
-            <h2 class="title is-5">{{ item.product.title }}</h2>
-            <p class="subtitle is-6">
-              <select v-model="item.quantity">
-                <option v-for="n in 10" :key="n" :value="n">
-                  {{ n }}
-                </option>
-              </select>
-              x $ {{ item.product.price }} = ${{
-                item.product.price * item.quantity
-              }}
-              <button
-                class="button is-danger is-small"
-                @click="removeFromCart(item.product)"
-              >
-                <div class="icon">
-                  <i class="fas fa-trash"></i>
-                </div>
-              </button>
-            </p>
-          </div>
+
+    <div class="cart-items">
+      <div v-for="item in cart" :key="item.product.id" class="item">
+        <img :src="item.product.thumbnail" :alt="item.product.title" />
+        <div class="item-details">
+          <h2 class="title is-5">{{ item.product.title }}</h2>
+          <p class="subtitle is-6">
+            <select v-model="item.quantity">
+              <option v-for="n in 10" :key="n" :value="n">{{ n }}</option>
+            </select>
+            x $ {{ item.product.price }} = ${{
+              item.product.price * item.quantity
+            }}
+            <button
+              class="button is-danger is-small"
+              @click="removeFromCart(item.product)"
+            >
+              <div class="icon">
+                <i class="fas fa-trash"></i>
+              </div>
+            </button>
+          </p>
         </div>
       </div>
     </div>
@@ -50,21 +47,23 @@ const cart = getCart();
   padding: 1rem;
   overflow-y: scroll;
 }
+
 .cart-items {
   display: flex;
   flex-direction: column;
 }
 .item {
-    display: flex;
-    flex-direction: row;
-    margin: 5rem;
-    border: 1px solid #ccc;
-    border-radius: 1rem;
-    box-shadow: 0 0 1rem #0004;
-    overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  margin: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+  box-shadow: 0 0 1rem #0004;
+  overflow: hidden;
 }
+
 .item img {
-    width: 100px;
-    margin-right: .5rem;
+  width: 100px;
+  margin-right: 0.5rem;
 }
 </style>
