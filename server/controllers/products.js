e0e9b2606c4399191f69809766b49bafce488b6b
jackgusler <guslerj1@newpaltz.edu>
2023-11-06 14:20:55 -0500

    worked on product.js

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,27 +1,34 @@
-const express = require('express');
-const { getProducts, getProductById, search } = require('../models/products');
-const router = express.Router();
+const express = require('express')
+const { getAll, get, search, create, update, remove } = require('../models/products')
+const router = express.Router()
 
 router.get('/', (req, res, next) => {
-
-    res.send(getProducts());
-
+  res.send(getAll())
 })
-.get('/search' , (req, res, next) => {
+  .get('/search', (req, res, next) => {
+    const results = search(req.query.q)
+    res.send(results)
+  })
+  .get('/:id', (req, res, next) => {
+    const product = get(+req.params.id)
+    if (product) {
+      res.send(product)
+    } else {
+      res.status(404).send({ error: 'Product not found' })
+    }
+  })
+  .post('/', (req, res, next) => {
+    const product = create(req.body)
+    res.send(product)
+  })
+  .patch('/:id', (req, res, next) => {
+    req.body.id = +req.params.id
+    const product = update(req.body)
+    res.send(product)
+  })
+  .delete('/:id', (req, res, next) => {
+    remove(+req.params.id)
+    res.send({ message: 'Product removed' })
+  })
 
-    const results = search(req.query.q);
-    res.send(results);
-
-})
-.get('/:id', (req, res, next) => {
-
-  const product = getProductById(+req.params.id);
-  if(product) {
-    res.send( product );
-  }else {
-    res.status(404).send({error: 'Product not found'});
-  }
-
-})
-
-module.exports = router;
+module.exports = router
--- server/models/products.js
+++ server/models/products.js
@@ -13,24 +13,24 @@
  * @property {string[]} images - The product's images.
  */
 
-const data = require("../data/products.json");
+const data = require("../data/products.json")
 
 /**
  * @returns {Product[]} An array of products.
  */
-function getProducts() {
-  return data.products;
+function getAll() {
+  return data.products
 }
 
 /**
  * @param {number} id - The product's ID.
  */
-function getProductById(id) {
-  return data.products.find((product) => product.id === id);
+function get(id) {
+  return data.products.find((product) => product.id === id)
 }
 
-function getProductsByCategory(category) {
-  return data.products.filter((product) => product.category === category);
+function getByCategory(category) {
+  return data.products.filter((product) => product.category === category)
 }
 
 function search(query) {
@@ -38,10 +38,54 @@ function search(query) {
     return (
       product.title.toLowerCase().includes(query.toLowerCase()) ||
       product.description.toLowerCase().includes(query.toLowerCase())
-    );
-  });
+    )
+  })
+}
+
+/**
+ * @param {Product} product - The product to create.
+ * @returns {Product} The created product.
+ */
+
+function create(product) {
+  const newProduct = {
+    id: data.products.length + 1,
+    ...product,
+  }
+  data.products.push(newProduct)
+  return newProduct
+}
+
+/**
+ * @param {number} id - The product's ID.
+ * @param {Product} product - The product's new data.
+ * @returns {Product} The updated product.
+ */
+
+function update(product) {
+  const index = data.products.findIndex((p) => p.id === product.id)
+  if (index === -1) {
+    throw new Error("Product not found")
+  }
+  data.products[index] = {
+    ...data.products[index],
+    ...product,
+  }
+  return data.products[index]
+}
+
+/**
+ * @param {number} id - The product's ID.
+ */
+
+function remove(id) {
+  const index = data.products.findIndex((product) => product.id === id)
+  if (index === -1) {
+    throw new Error("Product not found")
+  }
+  data.products.splice(index, 1)
 }
 
 module.exports = {
-  getProducts, getProductById, getProductsByCategory, search
-};
+  getAll, get, getByCategory, search, create, update, remove
+}
